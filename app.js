e6d9d0c790d5d8cdc3cc5d5f9d412c6629e75fa9
Maximo Martinez Gonzalez <maximomrtnz@gmail.com>
2017-03-26 21:50:38 -0300

    Server and Client Changes

--- app.js
+++ app.js
@@ -15,16 +15,44 @@ console.log('Server started.');
 SOCKET_LIST = {}
 
 var io = require('socket.io')(serv,{});
+
 io.sockets.on('connection',function(socket){
-	
-	console.log('socket connection');
 
-	socket.on('happy',function(data){
-		console.log('happy because '+data.reason);
-	});
+	socket.id = Math.random();
 
-	socket.emit('serverMsg',{
-		msg:'hello' 
+	socket.x = 0;
+	socket.y = 0;
+	socket.number = "" + Math.floor(10*Math.random());
+
+	SOCKET_LIST[socket.id] = socket;
+
+
+	socket.on('disconnect',function(){
+		delete SOCKET_LIST[socket.id];
 	});
 
 });
+
+
+setInterval(function(){
+	var pack = [];
+	
+	for(var i in SOCKET_LIST){
+		var socket = SOCKET_LIST[i];
+		socket.x++;
+		socket.y++;
+		pack.push({
+			x:socket.x,
+			y:socket.y,
+			number:socket.number
+		});
+	}
+
+	for(var i in SOCKET_LIST){
+		
+		var socket = SOCKET_LIST[i];
+		
+		socket.emit('newPositions',pack);
+
+	}
+},1000/25);
